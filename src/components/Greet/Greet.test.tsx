e0527a3c76f6bb827bbaeb87f7f9f5b337c8e6ec
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-03-17 20:08:05 +0600

    put the tests into a group

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -3,17 +3,20 @@
  * then it should render hello followed by the name.
  */
 
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet renders correctly", ()=> {
-    render(<Greet />);
-    const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument();
-})
+describe("Greet", () => {
 
-test("Greet renders Hello with a name", ()=> {
-    render(<Greet name="John" />);
-    const textElement = screen.getByText("Hello John");
-    expect(textElement).toBeInTheDocument();
+    test("renders correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+    })
+
+    test("renders Hello with a name", () => {
+        render(<Greet name="John" />);
+        const textElement = screen.getByText("Hello John");
+        expect(textElement).toBeInTheDocument();
+    })
 })
